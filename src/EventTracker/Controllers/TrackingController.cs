d254bdead35d09aacebceda042b58f1469577854
wk-j <somnuk.wk@gmail.com>
2018-07-04 01:05:09 +0700

    Removed value controller

--- src/EventTracker/Controllers/TrackingController.cs
+++ src/EventTracker/Controllers/TrackingController.cs
@@ -1,10 +1,24 @@
+using EventTracker.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace EventTracker.Controllers {
-    public class TrackingController {
+    public class Result {
+        public bool Success { set; get; }
+    }
+
+    public class TrackingController : ControllerBase {
         private readonly ILogger<TrackingController> logger;
         public TrackingController(ILogger<TrackingController> logger) {
             this.logger = logger;
+        }
+
+        public Result NewMouseEvent([FromBody] MouseEvent evt) {
+            return new Result { Success = true };
+        }
+
+        public Result NewKeyPressEvent([FromBody] KeyPressEvent evt) {
+            return new Result { Success = true };
         }
     }
 }
